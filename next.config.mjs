288b824e350c963ed3a3df82fd3fdc3b14c2a992
KwaHash <cosmosray001@gmail.com>
2025-10-30 07:50:14 +0900

    fix: disable image optimization for domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true, // Disable Next.js image optimization
+    domains: ["noshi2.com"],
+  },
+};
 
 export default nextConfig;
